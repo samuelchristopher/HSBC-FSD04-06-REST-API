9ea49551ea65a6299491c23f3b03ae1117bbd19f
samuelchristopher <samuel.christopher.santo@gmail.com>
2023-02-10 11:20:45 +0700

    feat: add HTTP METHOD DELETE

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,27 +1,5 @@
 const { user } = require('../models');
 
-exports.getUsers = async (req, res, next) => {
-  try {
-    // res.send({
-    //   nama: 'Sam',
-    //   alamat: 'Tangerang',
-    //   pekerjaan: 'Software Developer',
-    //   umur: 27,
-    // });
-
-    const users = await user.findAll();
-    // console.log(users);
-
-    return res.status(200).send({
-      message: `retrieve all data successfully`,
-      data: users,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send(error);
-  }
-};
-
 // exports.createUser = async (req, res, next) => {
 //   try {
 //     const payload = req.body;
@@ -48,6 +26,28 @@ exports.getUsers = async (req, res, next) => {
 //   }
 // };
 
+exports.getUsers = async (req, res, next) => {
+  try {
+    // res.send({
+    //   nama: 'Sam',
+    //   alamat: 'Tangerang',
+    //   pekerjaan: 'Software Developer',
+    //   umur: 27,
+    // });
+
+    const users = await user.findAll();
+    // console.log(users);
+
+    return res.status(200).send({
+      message: `retrieve all data successfully`,
+      data: users,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(error);
+  }
+};
+
 exports.aboutMe = async (req, res, next) => {
   try {
     // res.send({
@@ -62,6 +62,71 @@ exports.aboutMe = async (req, res, next) => {
       message: `retrieve data profile successfully`,
       data: me.dataValues,
     });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(error);
+  }
+};
+
+exports.getUserById = async (req, res, next) => {
+  try {
+    const data = await user.findOne({
+      where: { id: req.params.id },
+    });
+    // console.log(typeof data);
+    if (data !== null) {
+      return res.status(200).send({
+        message: `retrieve data by ID successfully`,
+        data: data.dataValues,
+      });
+    } else {
+      return res.status(404).send({
+        message: `ID Not Found`,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(error);
+  }
+};
+
+exports.updateUserById = async (req, res, next) => {
+  try {
+    const data = await user.update({
+      where: { id: req.params.id },
+    });
+    // console.log(typeof data);
+    if (data !== null) {
+      return res.status(200).send({
+        message: `Update data by ID successfully`,
+        data: data.dataValues,
+      });
+    } else {
+      return res.status(404).send({
+        message: `ID Not Found`,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(error);
+  }
+};
+exports.deleteUserById = async (req, res, next) => {
+  try {
+    const data = await user.destroy({
+      where: { id: req.params.id },
+    });
+    // console.log(typeof data);
+    if (data !== null) {
+      return res.status(200).send({
+        message: `Delete data by ID successfully`,
+        data: data.dataValues,
+      });
+    } else {
+      return res.status(404).send({
+        message: `ID Not Found`,
+      });
+    }
   } catch (error) {
     console.error(error);
     return res.status(500).send(error);
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -7,5 +7,8 @@ const router = express.Router();
 
 router.get('/', userController.getUsers);
 router.get('/me', verifyTokenMiddleware.verifyToken, userController.aboutMe);
+router.get('/:id', userController.getUserById);
+// router.put('/:id', userController.updateUserById);
+router.delete('/:id', userController.deleteUserById);
 
 module.exports = router;
